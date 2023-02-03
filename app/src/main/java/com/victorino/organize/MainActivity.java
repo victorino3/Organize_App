9c9b56eae4213575ca3856ee4421572f106579f4
victorino3 <victorinogomes1@gmail.com>
2023-02-03 19:47:12 +0000

    slides scripts

--- app/src/main/java/com/victorino/organize/MainActivity.java
+++ app/src/main/java/com/victorino/organize/MainActivity.java
@@ -4,11 +4,42 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.heinrichreimersoftware.materialintro.app.IntroActivity;
+import com.heinrichreimersoftware.materialintro.slide.FragmentSlide;
+
+public class MainActivity extends IntroActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_main);
+        setButtonBackVisible(false);
+        setButtonNextVisible(false);
+        setPageScrollDuration(10);
+        setFullscreen(true);
+        addSlide(new FragmentSlide.Builder()
+                .background(R.color.white)
+                .backgroundDark(R.color.black)
+                .fragment(R.layout.intro_1)
+                .build()
+        );
+        addSlide(new FragmentSlide.Builder()
+                .background(R.color.white)
+                .backgroundDark(R.color.black)
+                .fragment(R.layout.intro_2)
+                .build()
+        );
+        addSlide(new FragmentSlide.Builder()
+                .background(R.color.white)
+                .backgroundDark(R.color.black)
+                .fragment(R.layout.intro_3)
+                .build()
+        );
+        addSlide(new FragmentSlide.Builder()
+                .background(R.color.white)
+                .backgroundDark(R.color.black)
+                .fragment(R.layout.intro_4)
+                .build()
+        );
     }
 }
