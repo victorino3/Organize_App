444892d5aceb112e8af7ba6b1cca14f169eb4f7a
victorino3 <victorinogomes1@gmail.com>
2023-02-07 13:37:13 +0000

    register and login scripts done

--- app/src/main/java/com/victorino/organize/activity/MainActivity.java
+++ app/src/main/java/com/victorino/organize/activity/MainActivity.java
@@ -1,11 +1,14 @@
-package com.victorino.organize;
+package com.victorino.organize.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.heinrichreimersoftware.materialintro.app.IntroActivity;
 import com.heinrichreimersoftware.materialintro.slide.FragmentSlide;
+import com.victorino.organize.R;
 
 public class MainActivity extends IntroActivity {
 
@@ -39,7 +42,23 @@ public class MainActivity extends IntroActivity {
                 .background(R.color.white)
                 .backgroundDark(R.color.black)
                 .fragment(R.layout.intro_4)
+                //.canGoForward(false)
                 .build()
         );
+        addSlide(new FragmentSlide.Builder()
+                .background(R.color.white)
+                .backgroundDark(R.color.black)
+                .fragment(R.layout.intro_cadastro)
+                .canGoForward(false)
+                .build()
+        );
+    }
+
+    public void registerUser(View view){
+        startActivity(new Intent(this,RegisterActivity.class));
+    }
+
+    public void haveCount(View view){
+        startActivity(new Intent(this,LoginActivity.class));
     }
 }
---
+++ app/src/main/java/com/victorino/organize/activity/RegisterActivity.java
@@ -0,0 +1,16 @@
+package com.victorino.organize.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.victorino.organize.R;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastro);
+    }
+}
