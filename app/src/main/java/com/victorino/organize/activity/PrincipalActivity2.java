65db0e73f9b0b2d225ecd2530adcba4323d140d5
victorino3 <victorinogomes1@gmail.com>
2023-02-16 12:57:32 +0000

    adding firebase to handle authentication

--- app/src/main/java/com/victorino/organize/activity/PrincipalActivity2.java
+++ app/src/main/java/com/victorino/organize/activity/PrincipalActivity2.java
@@ -35,13 +35,13 @@ public class PrincipalActivity2 extends AppCompatActivity {
         appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
 
-        binding.fab.setOnClickListener(new View.OnClickListener() {
+       /* binding.fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
-        });
+        });*/
     }
 
     @Override
