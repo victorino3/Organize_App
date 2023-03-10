26948dc6d84ceb41d6ad02168ebba60212c46918
victorino3 <victorinogomes1@gmail.com>
2023-03-10 14:19:15 +0000

    adding more layout, logout optons with menu

--- app/src/main/java/com/victorino/organize/activity/PrincipalActivity2.java
+++ app/src/main/java/com/victorino/organize/activity/PrincipalActivity2.java
@@ -2,27 +2,34 @@ package com.victorino.organize.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
-
-import com.google.android.material.snackbar.Snackbar;
-
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.TextView;
 
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
+import androidx.recyclerview.widget.RecyclerView;
 
-
+import com.google.firebase.auth.FirebaseAuth;
+import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
 import com.victorino.organize.R;
+import com.victorino.organize.config.FirebaseConfig;
 import com.victorino.organize.databinding.ActivityPrincipal2Binding;
 
 public class PrincipalActivity2 extends AppCompatActivity {
 
     private AppBarConfiguration appBarConfiguration;
     private ActivityPrincipal2Binding binding;
+    private MaterialCalendarView calendarView;
+    private TextView textCurrentMoney;
+    private TextView greet;
+    private RecyclerView recyclerView;
+
 
 
     @Override
@@ -37,9 +44,19 @@ public class PrincipalActivity2 extends AppCompatActivity {
                 WindowManager.LayoutParams.FLAG_HARDWARE_ACCELERATED,
                 WindowManager.LayoutParams.FLAG_HARDWARE_ACCELERATED);
 
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_principal2);
+        recyclerView = findViewById(R.id.recyclerView);
+        calendarView = findViewById(R.id.calendar);
+        greet = findViewById(R.id.textgreet);
+        textCurrentMoney = findViewById(R.id.textSaldo);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar.setTitle("Organize");
+        setSupportActionBar(toolbar);
+        configCalendar();
+        greetUser();
+
+        /*NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_principal2);
         appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);*/
 
        /* binding.fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -49,6 +66,26 @@ public class PrincipalActivity2 extends AppCompatActivity {
             }
         });*/
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.menuLogout:
+                FirebaseAuth logoutUser = FirebaseConfig.getAuth();
+                logoutUser.signOut();
+                startActivity(new Intent(this,MainActivity.class));
+                finish();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
     public void AddExpense(View view){
         startActivity(new Intent(this,DespesaActivity.class));
     }
@@ -58,11 +95,11 @@ public class PrincipalActivity2 extends AppCompatActivity {
     public void AiSuggestion(View view){
         //Ai snippets
     }
-
-    @Override
-    public boolean onSupportNavigateUp() {
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_principal2);
-        return NavigationUI.navigateUp(navController, appBarConfiguration)
-                || super.onSupportNavigateUp();
+    public void greetUser(){}
+    public void configCalendar(){
+        String[] month = {"Janeiro","Feverreiro","Março","Abril","Maio","Junho","Julho","Agosto","Setembro","Outubro","Novembro","Dezembro"};
+        calendarView.setTitleMonths(month);
     }
+
+
 }
